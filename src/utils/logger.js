94ac2be28e7d15adce6255e74b40d3c6b438f18b
JulianaS2z <juliana.e.santos@ba.estudante.senai.br>
2026-05-24 20:57:55 -0300

    Estrutura do back-end

---
+++ src/utils/logger.js
@@ -0,0 +1,11 @@
+export function info(...args) {
+  console.info('[INFO]', ...args);
+}
+
+export function warn(...args) {
+  console.warn('[WARN]', ...args);
+}
+
+export function error(...args) {
+  console.error('[ERROR]', ...args);
+}
